011d2275082711de4bdaf8a44c0a741ed7137f37
Rana Mahad Ahmer <ranamahadahmer@outlook.com>
2024-07-23 21:11:13 +0500

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    import img from '$lib/images/monkey.jpeg';
+</script>
+
+
+<h1 style="text-align: center">Welcome to my Website</h1>
+<p style="text-align: center">I am Hafiz Ahmed</p>
+<div style="text-align: center" ><img style="width: 30%;" src={img} alt="Image"/></div>
+
+
