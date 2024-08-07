6c6045da617bb3b1219d9d9bdd784bb8bda58efb
Rana Mahad Ahmer <108465680+RanaMahadAhmer@users.noreply.github.com>
2024-08-07 15:03:24 +0500

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 
 
 <h1 style="text-align: center">Welcome to my Website</h1>
-<p style="text-align: center">I am Hafiz Ahmed</p>
+<p style="text-align: center">I am Nanga Ahmed</p>
 <div style="text-align: center" ><img style="width: 30%;" src={img} alt="Image"/></div>
 
 
